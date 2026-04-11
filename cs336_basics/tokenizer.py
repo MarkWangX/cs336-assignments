4f06a1cbd1321e715e3fb07278884462e0a43370
MarkWang <yw149@illinois.edu>
2026-04-11 21:19:13 +0800

    save local changes

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -4,6 +4,7 @@ from typing import BinaryIO
 from collections import Counter, defaultdict
 from multiprocessing import Pool
 from tqdm import tqdm
+import pickle
 ###############################################################
 # find_chunk_boundaries will receive a file and 
 # return a list of int that indicates the boundaries of chunks 
@@ -185,5 +186,84 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]):
         cur_vocab_size += 1
 
         pbar.update(1)
-    
     return vocab, merges
+
+class Tokenizer:
+    def __init__(self, vocab, merges, special_tokens=None):
+        self.vocab = vocab.copy()
+        self.merges = merges
+        self.special_tokens = special_tokens
+        vocab_size = max(self.vocab.keys()) + 1
+        existing_values = set(self.vocab.values())
+        if special_tokens is not None:
+            for special_token in special_tokens:
+                special_token = special_token.encode("utf-8")
+                if special_token not in existing_values:
+                    self.vocab[vocab_size] = special_token
+                    existing_values.add(special_tokens)
+                    vocab_size += 1
+        self.token_to_ids = {value: key for key, value in self.vocab.items()}
+        self.merge_dict = {pair: i for i, pair in enumerate(self.merges)}
+
+    @classmethod
+    def from_files(cls, vocab_filepath, merges_filepath, special_tokens=None):
+        with open(vocab_filepath, "rb") as f:
+            vocab = pickle.load(f)
+
+        with open(merges_filepath, "rb") as f:
+            merges = pickle.load(f)
+
+        return cls(vocab=vocab, merges=merges, special_tokens=special_tokens)
+
+    def encode(self, text:str) -> list[int]:
+        standard_pattern = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+        
+        if hasattr(self, 'special_tokens') and self.special_tokens:
+            special_pattern = "|".join(re.escape(tok) for tok in self.special_tokens)
+            final_pattern = f"({special_pattern}|{standard_pattern})"
+        else:
+            final_pattern = standard_pattern
+
+        compiled_pat = re.compile(final_pattern)
+        pre_tokens = compiled_pat.findall(text)
+
+        for word in pre_tokens:
+            if hasattr(self, 'special_tokens') and self.special_tokens and word in self.special_tokens:
+                word_bytes = word.encode('utf-8')
+                out_ids.append(self.token_to_ids[word_bytes])
+                continue
+
+            out_ids = []
+            word_byte = [bytes([b]) for b in word.encode("utf-8")]
+            while len(word_byte) > 1:
+                pairs = list(zip(word_byte[:-1], word_byte[1:]))
+
+                valid_pairs = [pair for pair in pairs if pair in self.merge_dict]
+                if not valid_pairs:
+                    break
+                best_pairs = min(valid_pairs, key = lambda x: valid_pairs[x])
+                first, second = best_pairs
+                
+                i = 1
+                new_word = []         
+                while i < len(word_byte):
+                        if i < len(word_byte) - 1 and word_byte[i] == first and word_byte[i+1] == second:
+                            new_word.append(first + second)
+                            i += 2
+                            break
+                        else:
+                            new_word.append(word[i])
+                            i += 1
+                word_byte = new_word
+
+        for b in new_word:
+            out_ids.append(self.token_to_ids[b])
+
+        return out_ids
+
+    def decode(self, ids: list[int]) -> str:
+        tokens = []
+        for id in ids:
+            tokens.append(self.vocab[id])
+        return tokens
+  
